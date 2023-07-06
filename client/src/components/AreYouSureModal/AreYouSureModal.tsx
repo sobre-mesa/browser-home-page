728f71cdd818ba09a6d2f0718a0e88d7009e51bb
Nicolas Videla <videla.jn@gmail.com>
2023-07-06 19:05:05 -0300

    AreYouSureModal code cleaned up

--- client/src/components/AreYouSureModal/AreYouSureModal.tsx
+++ client/src/components/AreYouSureModal/AreYouSureModal.tsx
@@ -1,32 +1,7 @@
 import React from 'react';
 import { Box, Button, Modal, Typography } from '@mui/material';
 import { Dispatch, SetStateAction } from 'react';
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor:  'rgba(0, 0, 0, 0.8)',
-    borderRadius: 5,
-    p: 4,
-};
-
-const closeButtonStyle = {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    color: '#000',
-    backgroundColor: '#fff',
-    borderRadius: '50%',
-    width: 24,
-    height: 24,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    cursor: 'pointer',
-};
+import './AreYouSureModal.css';
 
 type AreYouSureModalProps = {
   open: boolean;
@@ -36,25 +11,44 @@ type AreYouSureModalProps = {
   message: string;
 };
 
+
 export const AreYouSureModal = ({ open, setOpen, onConfirm, title, message }: AreYouSureModalProps) => {
     const handleClose = () => setOpen(false);
-
+    const CloseButton = () => {
+        return (
+            <div className="closeButton"
+                onClick={handleClose}>
+                X
+            </div>
+        );
+    };
     return (
-        <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-            <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2" color="#fff">
+        <Modal 
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description">
+            <Box className="areYouSureModal">
+                <Typography 
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                    color="#fff">
                     {title}
                 </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }} color="#fff">
+                <Typography
+                    id="modal-modal-description"
+                    sx={{ mt: 2 }} 
+                    color="#fff">
                     {message}
                 </Typography>
-                <Button variant="contained" onClick={onConfirm} sx={{ mt: 2, backgroundColor: 'red' }}>
-          Confirm
-                    <span style={{ marginLeft: '0.5rem', color: '#000' }}>X</span>
+                <Button
+                    variant="contained"
+                    onClick={onConfirm}
+                    sx={{ mt: 2, backgroundColor: 'red' }}>
+                    Confirm
                 </Button>
-                <div style={closeButtonStyle} onClick={handleClose}>
-          X
-                </div>
+                <CloseButton />
             </Box>
         </Modal>
     );
