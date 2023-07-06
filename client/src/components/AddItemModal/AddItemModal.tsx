140dcb9e43063a36154e69da8379e2c82385fa65
Nicolas Videla <videla.jn@gmail.com>
2023-07-06 18:56:41 -0300

    Add Item Code Cleaned up

--- client/src/components/AddItemModal/AddItemModal.tsx
+++ client/src/components/AddItemModal/AddItemModal.tsx
@@ -6,31 +6,21 @@ import { addSavedItem, updateSavedItem, selectUser } from '../../store/slices/da
 import { SystemCategory } from '../../models/Store';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import EditIcon from '@mui/icons-material/Edit';
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '78%',
-    transform: 'translate(-50%, -50%)',
-    maxWidth: 400,
-    bgcolor: 'rgba(0, 0, 0, 0.8)',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-    borderRadius: 3,
-};
-
-const titleStyle = {
-    color: 'rgba(255, 255, 255, 0.7)',
-    padding: '10px',
-    marginBottom: '10px',
-};
+import './AddItemModal.css';
 
 type AddOrEditItemModalProps = {
   open: boolean;
   handleClose: () => any;
   category: SystemCategory;
   itemToEdit?: SavedItem | null;
+};
+
+const ImageNotFound = () => {
+    return (
+        <Typography variant="body2" sx={{ color: 'white', marginBottom: '10px' }}>
+        Image not found
+        </Typography>
+    ); 
 };
 
 
@@ -40,34 +30,40 @@ export default function AddOrEditItemModal({
     category,
     itemToEdit,
 }: AddOrEditItemModalProps) {
-    const [description, setDescription] = useState(itemToEdit?.description || '');
-    const [url, setUrl] = useState(itemToEdit?.url || '');
-    const [image, setImage] = useState(itemToEdit?.image || '');
+    const [description, setDescription] = useState('');
+    const [url, setUrl] = useState('');
+    const [image, setImage] = useState('');
     const [imageError, setImageError] = useState(false);
     const user = useAppSelector(selectUser);
+
     useEffect(() => {
-        setDescription(itemToEdit?.description || '');
-        setUrl(itemToEdit?.url || '');
-        setImage(itemToEdit?.image || '');
-        setImageError(false);
+        if (itemToEdit) {
+            const { description, url, image } = itemToEdit;
+            setDescription(description);
+            setUrl(url);
+            setImage(image);
+            setImageError(false);
+        }
     }, [itemToEdit]);
 
     const dispatch = useAppDispatch();
 
     const payload = { category: category.name };
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
         const item: SavedItem = {
             description,
             url,
             image,
             category: category.id,
-            user
+            user,
         };
-        console.log(item);
+
         const action = itemToEdit
             ? updateSavedItem({ ...payload, item, id: itemToEdit.id as string })
             : addSavedItem({ ...payload, item });
-        e.preventDefault();
+
         dispatch(action);
         handleClose();
     };
@@ -88,11 +84,8 @@ export default function AddOrEditItemModal({
         }
     };
 
-    const isEditing = itemToEdit !== null;
-    const title =
-    (isEditing ? 'Editing ' + itemToEdit?.description + ' in ' : 'Add a new item to ') +
-    'category ' +
-    category.name;
+    const isEditing = Boolean(itemToEdit);
+    const title = `${isEditing ? 'Editing' : 'Add a new item to'} category ${category.name}`;
 
     return (
         <Modal
@@ -101,94 +94,58 @@ export default function AddOrEditItemModal({
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
-            <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2" sx={titleStyle}>
+            <Box className="addItemModal">
+                <Typography id="modal-modal-title" variant="h6" component="h6" className="addItemModalTitle">
                     {title}
                 </Typography>
-                {image && !imageError ? (
-                    <Avatar
-                        src={image}
-                        alt="Preview"
-                        sx={{
-                            width: 100,
-                            height: 100,
-                            marginBottom: '10px',
-                            backgroundColor: 'transparent',
-                        }}
-                        onError={handleImageLoadError}
-                    />
-                ) : (
-                    imageError && (
-                        <Typography variant="body2" sx={{ color: 'white', marginBottom: '10px' }}>
-                Image not found
-                        </Typography>
-                    )
-                )}
+                {
+                    (image && !imageError) ? (
+                        <Avatar
+                            src={image}
+                            alt="Preview"
+                            sx={{
+                                width: 100,
+                                height: 100,
+                                marginBottom: '10px',
+                            }}
+                            onError={handleImageLoadError}
+                        />
+                    ) : imageError && <ImageNotFound />
+                }
                 <form onSubmit={onSubmit}>
-                    <TextField
-                        label="Description"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        fullWidth
-                        margin="normal"
-                        variant="standard"
-                        InputProps={{
-                            style: {
-                                color: 'white',
-                                borderBottomColor: 'white',
-                            },
-                        }}
-                        InputLabelProps={{
-                            style: {
-                                color: 'white',
-                            },
-                        }}
-                    />
-                    <TextField
-                        label="URL"
-                        value={url}
-                        onChange={(e) => setUrl(e.target.value)}
-                        fullWidth
-                        margin="normal"
-                        variant="standard"
-                        InputProps={{
-                            style: {
-                                color: 'white',
-                                borderBottomColor: 'white',
-                            },
-                        }}
-                        InputLabelProps={{
-                            style: {
-                                color: 'white',
-                            },
-                        }}
-                    />
-                    <TextField
-                        label="Image"
-                        value={image}
-                        onChange={(e) => {
-                            setImage(e.target.value);
-                            setImageError(false);
-                        }}
-                        fullWidth
-                        margin="normal"
-                        variant="standard"
-                        InputProps={{
-                            style: {
-                                color: 'white',
-                                borderBottomColor: 'white',
-                            },
-                        }}
-                        InputLabelProps={{
-                            style: {
-                                color: 'white',
-                            },
-                        }}
-                        onPaste={handleImagePaste}
-                    />
-                    <div style={{ display: 'flex' }}>
-                        <IconButton style={{ marginLeft: 'auto' }} type="submit" color="error" aria-label="AddOrEdit">
-                            {isEditing ? <EditIcon fontSize="large" /> : <AddCircleOutlineIcon fontSize="large" />}
+                    {['Description', 'URL', 'Image'].map((field) => (
+                        <TextField
+                            key={field}
+                            label={field}
+                            value={field === 'Description' ? description : field === 'URL' ? url : image}
+                            onChange={(e) => {
+                                if (field === 'Description') setDescription(e.target.value);
+                                else if (field === 'URL') setUrl(e.target.value);
+                                else {
+                                    setImage(e.target.value);
+                                    setImageError(false);
+                                }
+                            }}
+                            fullWidth
+                            margin="normal"
+                            variant="standard"
+                            InputProps={{
+                                style: {
+                                    color: 'white',
+                                    borderBottomColor: 'white',
+                                },
+                            }}
+                            InputLabelProps={{
+                                style: {
+                                    color: 'white',
+                                },
+                            }}
+                            onPaste={field === 'Image' ? handleImagePaste : undefined}
+                        />
+                    ))}
+                    <div className="submitButtonContainer">
+                        <IconButton type="submit" color="error" aria-label="AddOrEdit">
+                            {isEditing ? <EditIcon /> : <AddCircleOutlineIcon />}
                         </IconButton>
                     </div>
                 </form>
