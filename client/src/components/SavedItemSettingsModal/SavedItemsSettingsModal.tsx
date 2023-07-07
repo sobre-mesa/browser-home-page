ab3616f031cacc19dfba13d0e3b649e609a538b9
Nicolas Videla <videla.jn@gmail.com>
2023-07-07 17:04:35 -0300

    Cleanup SavedItemSettingsModal

--- client/src/components/SavedItemSettingsModal/SavedItemsSettingsModal.tsx
+++ client/src/components/SavedItemSettingsModal/SavedItemsSettingsModal.tsx
@@ -9,6 +9,7 @@ import { SavedItemsSettingsItem } from '../SavedItemSettingsItem/SavedItemsSetti
 import AddItemModal from '../AddItemModal/AddItemModal';
 import { toggleSystemCategorySettings } from '../../store/slices/dataSlice';
 
+import './SavedItemSettingsModal.css';
 
 export const SavedItemsSettingsModal = ({ category }: { category: SystemCategory | StoreCategory}) => {
     const [itemToEdit, setItemToEdit] = useState(null);
@@ -16,99 +17,77 @@ export const SavedItemsSettingsModal = ({ category }: { category: SystemCategory
     const [modalOpen, setModalOpen] = useState(false);
     const dispatch = useDispatch();
 
-    const handleCloseModal = () => {
-        setModalOpen(false);
-    };
-    const handleOpenEditModal = () => {
-        setModalOpen(true);
-    };
+    const categoryName = category.name ===  'apps' || category.name === 'channels' ? category.name : 'custom';
+
+    const handleCloseModal = () => setModalOpen(false);
+    const handleOpenEditModal = () => setModalOpen(true);
+    const handleCloseSelf = () => dispatch(toggleSystemCategorySettings(categoryName));
+
     const handleOpenAddModal = () => {
         setItemToEdit(null);
         setModalOpen(true);
     };
-    const categoryName = category.name ===  'apps' || category.name === 'channels' ? category.name : 'custom';
-    const handleCloseSelf = () => {
-        dispatch(toggleSystemCategorySettings(categoryName));
-    };
   
-    useEffect(() => {
-        setItems(category.items);
-    }, [category]);
     
     const closeButtonSx = { position: 'absolute', top: '10px', right: '10px', zIndex: 1 };
     const closeIconSX = { width: 15, height: 15, marginRight: '-10px', marginTop: '-15px', color: 'rgba(255, 255, 255, 0.3)' };
     const addIconSX = { width: 20, height: 20, color: 'rgba(255, 255, 255, 0.3)', paddingBottom: '6px' };
-    const titleStyle = {
-        gridColumn: '1 / -1',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '20px',
-        color: 'white',
-        fontWeight: 'bold',
-        paddingTop: 6,
-        fontFamily: 'Cutive Mono'
-    };
     const paperstyle = {
-        outer: {
-            position: 'relative',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: categoryName === 'apps' ? 'rgba(0, 0, 0, 0.503)' : 'rgba(164, 30, 30, 0.7)',
-        },
-        inner: {
-            display: 'grid',
-            gridTemplateColumns: 'repeat(4, 1fr)',
-            gridTemplateRows: 'repeat(4, 1fr)',
-            padding: '10px',
-            height: 'calc(4 * 121px)', // Adjust the row limit as per your item's height
-            overflowY: 'auto',
-            backgroundColor: 'white',
-        }
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: categoryName === 'apps' ? 'rgba(0, 0, 0, 0.503)' : 'rgba(164, 30, 30, 0.7)',
     };
 
-    const noItemsStyle = {
-        outer: {width: 270, display: 'flex', justifyContent: 'center', alignItems: 'center', gridColumn: '1 / -1', gridRow: '1 / -1'},
-        inner: {color: 'rgba(0, 0, 0, 0.3)', fontSize: '16px'}
-    };
+    useEffect(() => {
+        setItems(category.items);
+    }, [category]);
 
-    return (
-        <Paper sx={paperstyle.outer}>
-            <span style={titleStyle}> {category.name.toUpperCase()}</span>
-            <IconButton
-                sx={closeButtonSx}
-                aria-label="close"
-                onClick={handleCloseSelf}
-            >
-                <CloseIcon sx={closeIconSX}/>
+    const CloseButton = () => (
+        <IconButton
+            sx={closeButtonSx}
+            aria-label="close"
+            onClick={handleCloseSelf}
+        >
+            <CloseIcon sx={closeIconSX}/>
+        </IconButton>
+    );
+
+    const AddItemButton = ()    => (
+        <div>
+            <IconButton aria-label="add" onClick={handleOpenAddModal}>
+                <AddCircleOutlineIcon sx={addIconSX} />
             </IconButton>
-            <div>
-                <IconButton aria-label="add" onClick={handleOpenAddModal}>
-                    <AddCircleOutlineIcon sx={addIconSX} />
-                </IconButton>
-            </div>
+        </div>
+    );
+    return (
+        <Paper sx={paperstyle}>
+            <span className="saved-items-modal-title"> {category.name.toUpperCase()}</span>
+            <CloseButton />
+            <AddItemButton />
             <AddItemModal
                 open={modalOpen}
                 handleClose={handleCloseModal}
                 category={category}
                 itemToEdit={itemToEdit}
             />
-            <Paper elevation={1} sx={paperstyle.inner}>
-                {items?.length > 0 ? items.map((item) => (
-                    <SavedItemsSettingsItem
-                        category={category.name}
-                        key={item.id}
-                        item={item}
-                        onEdit={setItemToEdit}
-                        handleOpen={handleOpenEditModal} 
-                    />
-                )) : 
-                    <div style={noItemsStyle.outer}>
-                        <div style={noItemsStyle.inner}>No items saved</div>
+            <div className="saved-items-modal-grid">
+                {items?.length > 0 ?
+                    items.map((item) => (
+                        <SavedItemsSettingsItem
+                            category={category.name}
+                            key={item.id}
+                            item={item}
+                            onEdit={setItemToEdit}
+                            handleOpen={handleOpenEditModal} 
+                        />
+                    )) :
+                    <div className='saved-items-modal-empty'>
+                        <span className="saved-items-modal-empty-text">No items saved</span>
                     </div>
                 }
-            </Paper>
+            </div>
         </Paper>
     );
 };
