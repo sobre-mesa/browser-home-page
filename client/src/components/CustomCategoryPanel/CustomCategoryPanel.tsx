72a74129939dd5094f2b07587259172016dbb131
Nicolas Videla <videla.jn@gmail.com>
2023-07-07 04:41:47 -0300

    Cleanup CustomCategoryPanel

--- client/src/components/CustomCategoryPanel/CustomCategoryPanel.tsx
+++ client/src/components/CustomCategoryPanel/CustomCategoryPanel.tsx
@@ -5,36 +5,39 @@ import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { toggleSystemCategorySettings } from '../../store/slices/dataSlice';
 import { useDispatch } from 'react-redux';
-const iconSx = { width: 18, height: 18, marginLeft: '-10px', marginTop: '-10px', color: 'rgba(255, 255, 255, 0.503)' };
-const warningStyle = {color: 'white',
-    height: 100,
-    backgroundColor: ' rgba(255, 255, 255,0.05)',
-    padding: 10, margin: 10,
-    borderRadius: 10,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 15,
-    fontWeight: 500,
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    cursor: 'pointer'};
+import './CustomCategoryPanel.css';
+const iconSx = { width: 18, height: 18, marginLeft: '-20px', marginTop: '-35px', color: 'rgba(255, 255, 255, 0.503)' };
+
 export const CustomCategoryPanel = ({ categories }: { categories: StoreCategory[] }) => {
     const dispatch = useDispatch();
-    const openSettings = () => {
-        dispatch(toggleSystemCategorySettings('custom'));
+    const openSettings = () => dispatch(toggleSystemCategorySettings('custom'));
+
+    const NoItemsYet = ({name} : {name: string}) => <div className="custom-category-warning"> No items added to {name} yet</div>;
+    const NoCategoriesYet = () =>  <div className="custom-category-warning no-categories-yet"> No categories added yet </div>;
+    const Categories = () => {
+        if(categories?.length > 0) {
+            return (
+                <>{ categories.map((c) => c?.items?.length > 0 ?
+                    <Category
+                        category={c.name}
+                        items={c.items}
+                        key={c.id} />
+                    : <NoItemsYet 
+                        key={c.id}
+                        name={c.name}/> )}</>
+            );
+        }
+        else {
+            return <NoCategoriesYet />;
+        }
     };
+
     return (
         <div style={{ padding: 20}}>
-            <IconButton onClick={openSettings} style={{marginTop: '-25px', marginLeft: '-10px'}}>
+            <IconButton onClick={openSettings}>
                 <SettingsIcon sx={iconSx}/>
             </IconButton>
-            {categories?.length > 0 ? 
-                ( categories.map((category) => category?.items?.length > 0 ?
-                    <Category key={category.id} category={category.name} items={category.items} /> :
-                    <div key={category.id} style={warningStyle}> No items added to {category.name} yet </div> ) ) :
-                ( <div style={{...warningStyle, height: '340px', backgroundColor: ' rgba(255, 255, 255,0.03)',}}>No categories added yet</div> )
-            }
+            <Categories/> 
         </div>
     );
 };
